c77d013b94b415711062d83e95109ee36658111d
dawid584 <109970770+dawid584@users.noreply.github.com>
2022-11-08 19:30:28 +0100

    fixed 5th exercise

--- Untitled-1.py
+++ Untitled-1.py
@@ -22,15 +22,46 @@ plt.show()
 ############################################################################
 
 data_0 = df.pivot_table(index='state', values='manner_of_death',aggfunc='count' )
+table_3= pd.DataFrame(data_0)
+
 data_1= pd.read_html('https://simple.wikipedia.org/wiki/List_of_U.S._states_by_population', header=0)
-d = pd.DataFrame(data_1[0])
+table_2 = pd.DataFrame(data_1[0])
+
+data_2= pd.read_html('https://en.wikipedia.org/wiki/List_of_U.S._state_and_territory_abbreviations', header=2)
+table_1= pd.DataFrame(data_2[0])
+
+table_1 = table_1['Unnamed: 3'].iloc[9:][:69]
+
+table_2 =table_2['Population estimate, July 1, 2019 [2]']
+
+table_3 =table_3['manner_of_death']
+
+len_1 = len(table_1)
+len_2 = len(table_2)
+len_3 = len(table_3)
+list_1 =[]
+list_2 =[]
+list_3 =[]
+
+for i in range(len_1):
+   list_1.append(i)
 
 
-left=pd.DataFrame({'Stan':d['State'] , "key": d['Rank in states & territories, 2019']  })
-right=pd.DataFrame({'Liczba ludności na 1.07.2019': d['Population estimate, July 1, 2019 [2]'] , "key": d['Rank in states & territories, 2019'] })
-right_2=pd.DataFrame({'Liczba zgonów': data_0['manner_of_death'], "key": d['Rank in states & territories, 2019']})
+for i in range(len_2):
+   list_2.append(i)
 
-table = pd.merge(left,right,how='right',on='key')
-table_2 = pd.merge(table,right_2,how='right',on='key')
-table_2['Liczba zgonów na 1000'] = (table_2['Liczba zgonów'] / table_2['Liczba ludności na 1.07.2019'] )*1000
-print(table_2)
+for i in range(len_3):
+   list_3.append(i)
+
+
+
+dictionary = {'kod stanu': table_1 , 'key': list_1 }
+dictionary= pd.DataFrame(dictionary)
+dictionary_2 = {'Wielkość populacji': table_2 , 'key': list_2  }
+dictionary_2 = pd.DataFrame(dictionary_2)
+state_table= pd.merge(dictionary ,dictionary_2 , how='inner' ,on='key')
+dictionary_3 = {'Wielkość populacji': table_3, 'key': list_3}
+dictionary_3 = pd.DataFrame(dictionary_3)
+state_table= pd.merge(state_table ,dictionary_3 , how='inner' ,on='key')
+state_table['Ilość zgonów na 1000']= (state_table['Wielkość populacji_y']/state_table['Wielkość populacji_x'])*1000
+print(state_table) 
